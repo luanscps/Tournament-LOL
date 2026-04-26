1468327da01bac11f197806bac586579bcaafa02
Luan <luanscps@gmail.com>
2026-04-26 17:20:55 -0300

    fix: adiciona pathname wildcard nos remotePatterns do next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,16 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "ddragon.leagueoflegends.com" },
-      { protocol: "https", hostname: "raw.communitydragon.org" },
+      {
+        protocol: "https",
+        hostname: "ddragon.leagueoflegends.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.communitydragon.org",
+        pathname: "/**",
+      },
     ],
   },
 };
